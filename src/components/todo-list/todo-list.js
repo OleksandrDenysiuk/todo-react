723d050b52eb5762b24d2abc1283e067bbde4da3
Oleksandr <denysiuk.oleksandr1997@gmail.com>
2020-07-24 20:01:37 +0200

    Refactor, add styles

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -1,5 +1,6 @@
 import React from "react";
-import TodoListItem from "./todo-list-item";
+import './todo-list.css';
+import TodoListItem from "../todo-list-item";
 
 const TodoList = ({todos}) => {
 
@@ -7,14 +8,14 @@ const TodoList = ({todos}) => {
 
         const {id, ...itemProps} = item;
         return (
-            <li key={id}>
+            <li key={id} className="list-group-item">
                 <TodoListItem {...itemProps}/>
             </li>
         );
     });
 
     return (
-        <ul>
+        <ul className="list-group todo-list">
             {elements}
         </ul>
     );
